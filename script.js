45662730f3f64bbcd727ea2f0dcf77cf09c9c555
Mubin Ansari <mubinansari910@gmail.com>
2022-08-17 16:09:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const apiKey = 'fcc8de7015bbb202209bbf0261babf4c';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+const fetchWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const iconUrl = 'https://openweathermap.org/img/wn/';
+const backgroundImageUrl = 'https://source.unsplash.com/1600x900/?';
